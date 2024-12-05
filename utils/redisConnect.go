28e3be3af1ccb72ffde2b0f9e369a76c4b667531
vanshikaChhikaraGit <vanshikachhikaraofficial@gmail.com>
2024-12-05 22:50:22 +0530

    typo fix

--- utils/redisConnect.go
+++ utils/redisConnect.go
@@ -1,19 +1,14 @@
 package utils
 
 import (
-	"fmt"
+	
 	"os"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/joho/godotenv"
 )
 
 func RedisClient()(*redis.Client){
-	err:=godotenv.Load()
-	if err!=nil{
-		fmt.Print("failed to load env variables")
-		return nil
-	}
+	
     rdb:= redis.NewClient(&redis.Options{
 		Addr: os.Getenv("REDIS_HOST"),
 		Password:os.Getenv("REDIS_PASSWORD"),
